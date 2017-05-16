6f49385711bbde2405fbada700a472f838e58696
syshen <sysheen@gmail.com>
2017-05-16 10:34:51 +0800

    Initial Commit

---
+++ china-map-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  china-map-test
+//
+//  Created by Shih Yun Shen on 16/05/2017.
+//  Copyright © 2017 Panobike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ china-map-test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  china-map-test
+//
+//  Created by Shih Yun Shen on 16/05/2017.
+//  Copyright © 2017 Panobike. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
